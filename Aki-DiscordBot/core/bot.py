d8a94365caf8bc867cf47e3e86752486898caa1f
Toil <ilya.t24@yandex.ru>
2021-07-18 01:13:34 +0300

    initial commit

--- Aki-DiscordBot/core/bot.py
+++ Aki-DiscordBot/core/bot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
 
+from scripts.parsers.settings import settings
 bot = commands.Bot(command_prefix = prefix, case_insensitive = True)
 bot.remove_command('help') # Удаляем встроенную команду хелп
