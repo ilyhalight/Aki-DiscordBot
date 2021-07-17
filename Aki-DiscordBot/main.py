1a99dd5474a9551acd60b2a226eb8dacebb10680
Toil <ilya.t24@yandex.ru>
2021-07-18 01:21:48 +0300

    Немного изменил текста

--- Aki-DiscordBot/main.py
+++ Aki-DiscordBot/main.py
@@ -4,7 +4,7 @@ import discord
 from dotenv import load_dotenv
 
 from core.bot import bot
-
+from scripts.console import clear
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
@@ -13,12 +13,13 @@ def do_env():
         if os.path.exists(dotenv_path):
             load_dotenv(dotenv_path)
     except:
-        print('Не удалось найти файл ".env".')
+        print('Не удалось найти ".env" файл :c')
 
 def run():
     bot.run(os.environ.get('DISCORD_TOKEN'))
 
 if __name__ == '__main__':
+    clear()
     do_env()
     run()
 
