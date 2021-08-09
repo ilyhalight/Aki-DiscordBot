a0d9e8c4653e40cb22fdb4f0837fa8b4c8e443ba
Toil <ilya.t24@yandex.ru>
2021-08-09 21:13:37 +0300

    Добавлен выввод ошибки при использовании букв

--- Aki-DiscordBot/cogs/random.py
+++ Aki-DiscordBot/cogs/random.py
@@ -1,11 +1,13 @@
+from inspect import Arguments
 import discord
 import random
 from discord.ext import commands
 
-from core.bot import avatar
+from core.bot import avatar, bot
 from core.logger import logger
 from data.colors import colors
 from scripts.parsers.settings import settings
+from scripts.parsers.imgs import imgs
 
 
 
@@ -32,6 +34,16 @@ class Random(commands.Cog):
         if count != None and count1 != None:
             await ctx.send(str(random.randint(int(count), int(count1))))
             logger.info(f'Рандомное число от {count} до {count1} - Пользователь: {ctx.author} ({ctx.author.id}).')
+
+    @random_command.error
+    async def random_command_error(self, ctx, error):
+        if isinstance (error, commands.BadArgument):
+            emb = discord.Embed(description = f'Использованы буквы в {settings["prefix"]}рандом\n (с) <@{ctx.author.id}>', color = colors['error'])
+            emb.set_footer(text = 'Aki © 2021 Все права защищены', icon_url = avatar(bot.user))
+            emb.set_author(name = 'Ошибка', icon_url = avatar(ctx.author))
+            emb.set_thumbnail(url = imgs['error'])
+            await ctx.send(embed = emb)
+            logger.error(f'Не удалось использовать команду random  - Причина: Использованы буквы - Пользователь: {ctx.author} ({ctx.author.id}).')
 
 def setup(bot):
     bot.add_cog(Random(bot))
