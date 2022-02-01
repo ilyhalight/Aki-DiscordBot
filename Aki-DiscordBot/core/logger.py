fbeaf9643c4241cae1bc058890cdd970c1cb0db8
ilyhalight <ilya.t24@yandex.ru>
2022-02-01 23:42:04 +0300

    v0.4.0 | Добавлена проверка на дебаг-мод

--- Aki-DiscordBot/core/logger.py
+++ Aki-DiscordBot/core/logger.py
@@ -1,13 +1,19 @@
 import os
+import sys
 from loguru import logger
 from scripts.parsers.settings import settings
 
 
 if settings is None:
     save_logs = False
+    debug = False
 else:
     save_logs = settings['save_logs']
+    debug = settings['debug']
 
+if not debug:
+    logger.remove(0)
+    logger.add(sys.stderr, level="INFO")
 
 if save_logs:
     if not os.path.isdir('./logs'):
