9118961ef112eb512b5c6b8842f305523c2bb938
Toil <ilyhalight2@gmail.com>
2022-02-02 18:31:38 +0300

    v0.4.0
    Добавлены подкоманды help, helper, новые варианты названия действий

--- Aki-DiscordBot/cogs/reactions.py
+++ Aki-DiscordBot/cogs/reactions.py
@@ -2,14 +2,13 @@ import discord
 
 from discord.ext import commands
 
-from core.bot import avatar
+from core.embeds import Helpers
 from core.logger import logger
-from data.colors import colors
 from scripts.parsers.settings import settings
 
-emoji_add = ["добавить"]
-emoji_del = ["удалить", "стереть", "очистить"]
-emoji_clear = ["удалить_всё", "стереть_всё", "очистить_всё", "удалить_все", "стереть_все", "очистить_все"]
+emoji_add = ["добавить", 'add']
+emoji_del = ["удалить", "стереть", "очистить", 'remove', 'clear', 'delete']
+emoji_clear = ["удалить_всё", "стереть_всё", "очистить_всё", "удалить_все", "стереть_все", "очистить_все", 'remove_all', 'clear_all', 'delete_all']
 
 class Reactions(commands.Cog):
     """Взаимодействие с эмоджи"""
@@ -17,34 +16,38 @@ class Reactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def reactions_help(self, prefix, emb: discord.Embed):
+        return emb.add_field(name = f'{prefix}реакция', value = 'Взаимодействие с реакциями', inline = False)
+
+    async def reactions_helper(self, ctx):
+        emb = await Helpers.default_embed(self, ctx, self.bot.user.avatar_url, 'Реакции')
+        emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}реакция <действие> <id сообщения> <реакция>`\n┗ Взаимодействие с реакциями', inline = False)
+        emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}реакция добавить 824428281104564264 :thumbsup:`\n┗ Добавит к сообщению с заданным айди реакцию :thumbsup:.', inline = False)
+        emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}реакция очистить 824428281104564264 :thumbsup:`\n┗ Очистит в сообщение с заданным айди реакцию :thumbsup:.', inline = False)
+        emb.add_field(name = 'Пример 3', value = f'`{settings["prefix"]}реакция очистить_все 824428281104564264`\n┗ Очистит в сообщение с заданным айди абсолютно все реакции.', inline = False)
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о "реакция" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+
     @commands.command(aliases = [
                                 'emoji', 'reaction', 'react',
                                 'эмодзи', 'эмоджи', 'реакция', 'реакт'
                                 ])
     @commands.has_permissions(administrator = True)
     async def reactions_command(self, ctx, act: str = None, id: int = None, reaction: str = None):
-        if act != None and id != None:
+        if act is not None and id is not None:
             await ctx.message.delete()
             message = await ctx.message.channel.fetch_message(id)
-            if act.lower() in emoji_add and reaction != None:
+            if act.lower() in emoji_add and reaction is not None:
                 await message.add_reaction(reaction)
-                logger.success(f'К сообщению "{message.id}" была добавлена реакция "{reaction}" - Пользователь: {ctx.author} ({ctx.author.id}).')
+                logger.success(f'К сообщению "{message.id}" была добавлена реакция "{reaction}" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
             if act.lower() in emoji_del and reaction != None:
                 await message.clear_reaction(reaction)
-                logger.success(f'В сообщение  "{message.id}" были очищены реакции "{reaction}" - Пользователь: {ctx.author} ({ctx.author.id}).')
+                logger.success(f'В сообщение "{message.id}" были очищены реакции "{reaction}" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
             if act.lower() in emoji_clear:
                 await message.clear_reactions()
-                logger.success(f'В сообщение "{message.id}" были очищены все реакции - Пользователь: {ctx.author} ({ctx.author.id}).')
+                logger.success(f'В сообщение "{message.id}" были очищены все реакции — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
         else:
-            emb = discord.Embed(title = 'Помощник - Реакции', color = colors['help'])
-            emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}реакция <действие> <id сообщения> <реакция>`', inline = False)
-            emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}реакция добавить 824428281104564264 :thumbsup:`\n┗ Добавит к сообщению с заданным айди реакцию :thumbsup:.', inline = False)
-            emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}реакция очистить 824428281104564264 :thumbsup:`\n┗ Очистит в сообщение с заданным айди определнные реакции :thumbsup:.', inline = False)
-            emb.add_field(name = 'Пример 3', value = f'`{settings["prefix"]}реакция очистить_все 824428281104564264`\n┗ Очистит в сообщение с заданным айди абсолютно все реакции.', inline = False)
-            emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            emb.set_thumbnail(url = avatar(self.bot.user))
-            await ctx.send(embed = emb)
-            logger.info(f'Информация о "реакция" - Пользователь: {ctx.author} ({ctx.author.id}).')
+            await self.reactions_helper(ctx)
 
 def setup(bot):
     bot.add_cog(Reactions(bot))
