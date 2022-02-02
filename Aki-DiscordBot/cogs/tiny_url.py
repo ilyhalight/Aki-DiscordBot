092a8bacabcb1a8e16fc8f1e2acc042f8e8769ae
Toil <ilyhalight2@gmail.com>
2022-02-02 18:33:12 +0300

    v0.4.0
    Добавлены подкоманды help, helper, удалены ненужные проверки

--- Aki-DiscordBot/cogs/tiny_url.py
+++ Aki-DiscordBot/cogs/tiny_url.py
@@ -1,17 +1,13 @@
-import traceback
 import discord
 import pyshorteners
 
 from discord.ext import commands
 
 from core.bot import avatar
+from core.embeds import Helpers
 from data.colors import colors
 from core.logger import logger
-try:
-    from scripts.parsers.imgs import imgs
-except ImportError:
-    logger.error('Не удалось загрузить модуль scripts/parsers/imgs.py - Пользователь: SYSTEM.')
-    logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+from scripts.parsers.imgs import imgs
 from scripts.parsers.settings import settings
 
 
@@ -21,32 +17,34 @@ class TinyUrl(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def tinyurl_help(self, prefix, emb: discord.Embed):
+        return emb.add_field(name = f'{prefix}тюрл', value = 'Сократить ссылку', inline = False)
+
+    async def tinyurl_helper(self, ctx):
+        emb = await Helpers.custom_image_embed(self, ctx, self.bot.user.avatar_url, 'tinyurl', 'Сокращение ссылок')
+        emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}тюрл <ссылка>`', inline = False)
+        emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}тюрл https://www.youtube.com`\n┗ Вернёт сокращенную ссылку для сайта "https://www.youtube.com".', inline = False)
+        emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}тюрл discord.com`\n┗ Вернёт сокращенную ссылку для сайта "https://discord.com".', inline = False)   
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о "тюрл" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+
     @commands.command(aliases = [
                                 'shortlink', 'surl', 'slink', 'shorturl', 'tinylink', 'turl', 'tlink', 'create_tiny_url',
                                 'шортлинк',  'сюрл', 'слинк', 'шортюрл',  'тинилинк', 'тюрл', 'тлинк', 'тиниюрл', 'сделать_короткую_ссылку', 'создать_короткую_ссылку'
                                 ])
     async def tinyurl_command(self, ctx, link: str = None):
-        if link == None:
-            emb = discord.Embed(title = 'Помощник - Сокращение ссылок', color = colors['help'])
-            emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}тюрл <ссылка>`', inline = False)
-            emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}тюрл https://www.youtube.com`\n┗ Вернёт сокращенную ссылку для сайта "https://www.youtube.com".', inline = False)
-            emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}тюрл discord.com`\n┗ Вернёт сокращенную ссылку для сайта "https://discord.com".', inline = False)
-            emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            if imgs:
-                emb.set_thumbnail(url = imgs['tinyurl'])
-            await ctx.send(embed = emb)
-            logger.info(f'Информация о "тюрл" - Пользователь: {ctx.author} ({ctx.author.id}).')
+        if link is None or type(link) is not str:
+            await self.tinyurl_helper(ctx)
         else:
             short_link = pyshorteners.Shortener().tinyurl.short(link)
             emb = discord.Embed(title = '', color = colors['help'])
-            emb.add_field(name = ':link:Первоначальная ссылка', value = f'⠀⠀{link}', inline = False)
-            emb.add_field(name = ':link:Сокращенная ссылка', value = f'⠀⠀{short_link}', inline = False)
+            emb.add_field(name = ':link:Первоначальная ссылка', value = f'⠀⠀{link}', inline = True)
+            emb.add_field(name = ':link:Сокращенная ссылка', value = f'⠀⠀{short_link}', inline = True)
             emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            emb.set_author(name = 'Ссылка успешно сокращена')
-            if imgs:
-                emb.set_thumbnail(url = imgs['tinyurl'])
+            emb.set_author(name = 'Ссылка успешно сокращена', icon_url = ctx.author.avatar_url)
+            emb.set_thumbnail(url = imgs['tinyurl'])
             await ctx.send(embed = emb)
-            logger.success(f'Ссылка "{link}" сокращена - Пользователь: {ctx.author} ({ctx.author.id}).')
+            logger.success(f'Ссылка "{link}" была сокращена - Пользователь: {ctx.author} ({ctx.author.id}).')
 
 def setup(bot):
     bot.add_cog(TinyUrl(bot))
