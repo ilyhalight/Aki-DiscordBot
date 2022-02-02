159c54029e1041a5e281936d29d4e8c72d5de9e6
Toil <ilyhalight2@gmail.com>
2022-02-02 18:20:12 +0300

    v0.4.0
    Добавлены подкоманды help, helper, красивый вывод ошибок, убраны не нужные проверки

--- Aki-DiscordBot/cogs/bot_info.py
+++ Aki-DiscordBot/cogs/bot_info.py
@@ -1,20 +1,12 @@
 import discord
-import traceback
 from discord.ext import commands
 
 from core.bot import avatar
+from core.embeds import Errors, Helpers
 from core.logger import logger
-try:
-    from scripts.parsers.owner import owner
-except ImportError:
-    logger.error('Не удалось загрузить модуль scripts/parsers/owner.py - Пользователь: SYSTEM.')
-    logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+from scripts.parsers.owner import owner
 from scripts.parsers.settings import settings
-try:
-    from scripts.parsers.info import info
-except ImportError:
-    logger.error('Не удалось загрузить модуль scripts/parsers/info.py - Пользователь: SYSTEM.')
-    logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+from scripts.parsers.info import info
 from data.colors import colors
 
 class BotInfo(commands.Cog):
@@ -23,25 +15,36 @@ class BotInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def botinfo_help(self, prefix, emb: discord.Embed):
+        return emb.add_field(name = f'{prefix}бот', value = 'Информация о боте', inline = False)
+
+    async def botinfo_helper(self, ctx):
+        emb = await Helpers.default_embed(self, ctx, self.bot.user.avatar_url, 'Информация о боте')
+        emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}бот`\n┗ Выведет информацию о боте.', inline = False)
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о "Информация о боте" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+
     @commands.command(aliases = [
                                 'info', 'bot', 'bot_info', 'botinfo', 'information',
                                 'инфо', 'бот', 'бот_инфо', 'ботинфо', 'информация'
                                 ])
     async def botinfo_command (self, ctx):
-        try:
-            emb = discord.Embed(title = ctx.guild.name, description = f'Информация о боте **{self.bot.user.name}**.\nПодробнее о командах - `{settings["prefix"]}хелп`', color = colors['help'])
-            emb.add_field(name = f'<:creator:868465769707601920>Создатель:', value = f'⠀⠀{owner["name"]}#{owner["tag"]}', inline = True)
-            emb.add_field(name = f'<:license:868473632035340349>Лицензия:', value = '⠀⠀MIT License', inline = True)
-            emb.add_field(name = f'<:status:868474272950128650>Статус:', value = f'⠀⠀{info["status"]}', inline = True)
-            emb.add_field(name = f'<:version:868474836568145940>Версия:', value = f'⠀⠀{info["version"]}', inline = True)
-            emb.add_field(name = f'<:github:868478604365922314>GitHub:', value = f'⠀⠀[Тык](https://github.com/ilyhalight/Aki-DiscordBot)', inline = True)
-            emb.set_thumbnail(url = avatar(self.bot.user))
-            emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            await ctx.send(embed = emb)
-            logger.info(f'Информация о Боте - Пользователь: {ctx.author} ({ctx.author.id}).')
-        except NameError:
-            await ctx.send('Команда отключена из-за ошибки.')
-            logger.error(f'Модуль scripts/parsers/owner.py или scripts/parsers/info.py не загружен - Пользователь: {ctx.author} ({ctx.author.id}).')
-            logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+        emb = discord.Embed(title = ctx.guild.name, description = f'Информация о боте **{self.bot.user.name}**.\nПодробнее о командах - `{settings["prefix"]}хелп`', color = colors['help'])
+        emb.add_field(name = f'<:creator:868465769707601920>Создатель:', value = f'⠀⠀{owner["name"]}#{owner["tag"]}', inline = True)
+        emb.add_field(name = f'<:license:868473632035340349>Лицензия:', value = '⠀⠀MIT License', inline = True)
+        emb.add_field(name = f'<:status:868474272950128650>Статус:', value = f'⠀⠀{info["status"]}', inline = True)
+        emb.add_field(name = f'<:version:868474836568145940>Версия:', value = f'⠀⠀{info["version"]}', inline = True)
+        emb.add_field(name = f'<:github:868478604365922314>GitHub:', value = f'⠀⠀[Тык](https://github.com/ilyhalight/Aki-DiscordBot)', inline = True)
+        emb.set_thumbnail(url = avatar(self.bot.user))
+        emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о Боте — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+
+
+    @botinfo_command.error
+    async def botinfo_command_error(self, ctx, error):
+        await Errors.custom_msg_embed(self, ctx, error)
+        logger.error(error)
+
 def setup(bot):
     bot.add_cog(BotInfo(bot))
