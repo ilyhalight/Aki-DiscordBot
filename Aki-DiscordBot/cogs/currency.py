205561c8b4de3bd56de431259f0347e090cfb171
Toil <ilyhalight2@gmail.com>
2022-02-02 18:26:49 +0300

    v0.4.0
    Добавлены подкоманды help, helper, небольшие изменения текста

--- Aki-DiscordBot/cogs/currency.py
+++ Aki-DiscordBot/cogs/currency.py
@@ -3,18 +3,29 @@ import pytz
 
 from discord.ext import commands
 from datetime import datetime
+from core.embeds import Helpers
 
 from core.logger import logger
 from core.bot import avatar
 from scripts.parsers.currency import parse_cbr
+from scripts.parsers.settings import settings
 from data.colors import colors
 
 
 class Currency(commands.Cog):
-    """Курс валюты (доллар, евро) за сегодня"""
+    """Курс валюты (доллар, евро) на сегодня"""
 
     def __init__(self, bot):
         self.bot = bot
+
+    def currency_help(self, prefix, emb: discord.Embed):
+        return emb.add_field(name = f'{prefix}курс', value = 'Курс валюты на сегодня', inline = False)
+
+    async def cryptocurrency_helper(self, ctx):
+        emb = await Helpers.default_embed(self, ctx, self.bot.user.avatar_url, 'Курс валюты')
+        emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}курс`\n┗ Выведет информацию о курсах валюты', inline = False)
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о "Курсе валюты на сегодня" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
 
     @commands.command(aliases = [
                                 'currency', 'currency_course', 'currencycourse',
@@ -28,7 +39,7 @@ class Currency(commands.Cog):
         emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
         emb.set_thumbnail(url = avatar(self.bot.user))
         await ctx.send(embed = emb)
-        logger.info(f'Выведена информация о курсе валюты на сегодня - Пользователь: {ctx.author} ({ctx.author.id}).')
+        logger.info(f'Выведена информация о курсе валюты на сегодня — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
 
 def setup(bot):
     bot.add_cog(Currency(bot))
