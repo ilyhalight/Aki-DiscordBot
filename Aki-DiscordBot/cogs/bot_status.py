9dac75158e63a04c9368eb0742aa75d4ad6795d9
Toil <ilyhalight2@gmail.com>
2022-02-02 18:21:28 +0300

    v0.4.0
    Добавлены подкоманды help, helper, красивый вывод ошибок, убраны не нужные проверки

--- Aki-DiscordBot/cogs/bot_status.py
+++ Aki-DiscordBot/cogs/bot_status.py
@@ -4,19 +4,12 @@ import discord
 from discord.ext import commands
 
 from core.bot import avatar, is_owner
+from core.embeds import Errors, Helpers
 from data.colors import colors
 from core.logger import logger
 from scripts.parsers.settings import settings
-try:
-    from scripts.parsers.imgs import imgs
-except ImportError:
-    logger.error('Не удалось загрузить модуль scripts/parsers/imgs.py - Пользователь: SYSTEM.')
-    logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
-try:
-    from scripts.parsers.status import status, update_status
-except ImportError:
-    logger.error('Не удалось загрузить модуль scripts/parsers/status.py - Пользователь: SYSTEM.')
-    logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+from scripts.parsers.imgs import imgs
+from scripts.parsers.status import status, update_status
 
 
 class BotStatus(commands.Cog):
@@ -25,6 +18,19 @@ class BotStatus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def botstatus_help(self, prefix, emb: discord.Embed):
+        return emb.add_field(name = f'{prefix}бот\_статус', value = 'Изменить статус бота', inline = False)
+
+    async def botstatus_helper(self, ctx):
+        emb = await Helpers.default_embed(self, ctx, self.bot.user.avatar_url, 'Статус бота')
+        emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}бот_статус <статус> <активность> <ссылка> <текст>`\n┗ Сменит статус бота на заданный.', inline = False)
+        emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}бот_статус онлайн слушает None $help - осн. комманды`\n┗ Сменит статус бота на онлайн, активность на слушает, а текст на "$help - осн.комманды".', inline = False)
+        emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}бот_статус идле стримит https://www.twitch.tv/bratishkinoff $help - осн. комманды`\n┗ Сменит статус бота на идле, активность на стримит, а текст на "$help - осн.комманды".', inline = False)
+        emb.add_field(name = 'Статусы', value = f'Онлайн, оффлайн, идле, не_беспокоить, невидимка', inline = False)
+        emb.add_field(name = 'Активности', value = f'Играет, слушает, смотрит, стримит', inline = False)
+        await ctx.send(embed = emb)
+        logger.info(f'Выведена информация о "Статусе бота" — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+
     @commands.command(aliases = [
                                 'botstatus', 'bot_status',
                                 'ботстатус', 'бот_статус'
@@ -32,7 +38,7 @@ class BotStatus(commands.Cog):
     async def botstatus_command(self, ctx, new_status = None, new_active = None, stream_link = None, *, arg = None):
         if is_owner(ctx.author.id) is True:
             if all((new_active, new_status, stream_link, arg)):
-                if new_status.lower() in ['offline', 'оффлайн', 'оффлаин', 'офлайн', 'офлаин', 'не_в_сети']:
+                if new_status.lower() in ['offline', 'оффлайн', 'оффлаин', 'офлайн', 'офлаин', 'не_в_сети', 'офф']:
                     new_status = discord.Status.offline
                 elif new_status.lower() in ['idle', 'идле', 'идлэ', 'офлайн', 'офлаин', 'не_активен']:
                     new_status = discord.Status.idle
@@ -43,66 +49,53 @@ class BotStatus(commands.Cog):
                 else:
                     new_status = discord.Status.online
 
-                if new_active.lower() in ['listen', 'listening', 'слушает', 'слушать']:
+                if new_active.lower() in ['play', 'playing', 'играет', 'играть']:
+                    new_activity = discord.ActivityType.playing
+                elif new_active.lower() in ['listen', 'listening', 'слушает', 'слушать']:
                     new_activity = discord.ActivityType.listening
                 elif new_active.lower() in ['watch', 'watching', 'смотрит', 'смотреть']:
                     new_activity = discord.ActivityType.watching
-                else:
+                elif new_active.lower() in ['stream', 'streaming', 'стримит', 'стриметь']:
                     new_activity = discord.ActivityType.streaming
+                else:
+                    new_activity = discord.ActivityType.custom
 
                 if stream_link.startswith('www.') and new_activity == discord.ActivityType.streaming or stream_link.startswith('http') and new_activity == discord.ActivityType.streaming:
                     await self.bot.change_presence(status = new_status, activity = discord.Activity(name = arg, url = stream_link, type = new_activity))
-                    try:
-                        status['status'] = new_status[0]
-                        status['active'] = new_activity[1]
-                        status['link'] = stream_link
-                        status['text'] = arg
-                        update_status(status)
-                        logger.warning(f'Статус бота изменен - Пользователь: {ctx.author} ({ctx.author.id}).')
-                        logger.info(f'Новый Статус: {new_status}, Активность: {new_activity}, Ссылка: {stream_link}, Текст: {arg}')
-                    except NameError:
-                        logger.error(f'Модуль scripts/parsers/status.py не загружен - Пользователь: {ctx.author} ({ctx.author.id}).')
-                        logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+                    status['status'] = new_status[0]
+                    status['active'] = new_activity[1]
+                    status['link'] = stream_link
+                    status['text'] = arg
+                    update_status(status)
+                    logger.warning(f'Статус бота изменен — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+                    logger.info(f'Новый Статус: {new_status}, Активность: {new_activity}, Ссылка: {stream_link}, Текст: {arg}')
                 else:
                     await self.bot.change_presence(status = new_status, activity = discord.Activity(name = arg, type = new_activity))
-                    try:
-                        status['status'] = new_status[0]
-                        status['active'] = new_activity[1]
-                        status['link'] = stream_link
-                        status['text'] = arg
-                        update_status(status)
-                        logger.warning(f'Статус бота изменен - Пользователь: {ctx.author} ({ctx.author.id}).')
-                        logger.info(f'Новый Статус: {new_status}, Активность: {new_activity}, Ссылка: {stream_link}, Текст: {arg}')
-                    except NameError:
-                        logger.error(f'Модуль scripts/parsers/status.py не загружен - Пользователь: {ctx.author} ({ctx.author.id}).')
-                        logger.debug(f'Причина ошибки:\n{traceback.format_exc()}')
+                    status['status'] = new_status[0]
+                    status['active'] = new_activity[1]
+                    status['link'] = stream_link
+                    status['text'] = arg
+                    update_status(status)
+                    logger.warning(f'Статус бота изменен — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+                    logger.info(f'Новый Статус: {new_status}, Активность: {new_activity}, Ссылка: {stream_link}, Текст: {arg}')
             else:
-                emb = discord.Embed(title = 'Помощник - Бот статус', color = colors['help'])
-                emb.add_field(name = 'Использование', value = f'`{settings["prefix"]}бот_статус <статус> <активность> <ссылка> <текст>`', inline = False)
-                emb.add_field(name = 'Пример 1', value = f'`{settings["prefix"]}бот_статус онлайн слушает None $help - осн.комманды`\n┗ Сменит статус бота на онлайн, активность на слушает, а текст на "$help - осн.комманды".', inline = False)
-                emb.add_field(name = 'Пример 2', value = f'`{settings["prefix"]}бот_статус идле стримит https://www.twitch.tv/bratishkinoff $help - осн.комманды`\n┗ Сменит статус бота на идле, активность на стримит, а текст на "$help - осн.комманды".', inline = False)
-                emb.add_field(name = 'Статусы', value = f'Онлайн, оффлайн, идле, не_беспокоить, невидимка', inline = False)
-                emb.add_field(name = 'Активность', value = f'Онлайн, оффлайн, идле, не_беспокоить, невидимка', inline = False)
-                emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-                emb.set_thumbnail(url = avatar(self.bot.user))
-                await ctx.send(embed = emb)
-                logger.info(f'Информация о "бот_статус" - Пользователь: {ctx.author} ({ctx.author.id}).')
+                await self.botstatus_helper(ctx)
         elif is_owner(ctx.author.id) == 'Команда отключена из-за ошибки.':
-            emb = discord.Embed(description = f'Команда отключена из-за ошибки\n (с) <@{ctx.author.id}>', color = colors['error'])
-            emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            emb.set_author(name = 'Упс-с', icon_url = avatar(ctx.author))
-            if imgs:
-                emb.set_thumbnail(url = imgs['not_available'])
-            await ctx.send(embed = emb)
-            logger.error(f'Не удалось изменить статус - Причина: Команда отключена из-за ошибки - Пользователь: {ctx.author} ({ctx.author.id}).')
+            await Errors.not_available_embed(self, ctx)
+            logger.error(f'Не удалось изменить статус - Причина: Команда отключена из-за ошибки — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
         else:
-            emb = discord.Embed(description = f'Недостаточно прав\n (с) <@{ctx.author.id}>', color = colors['error'])
-            emb.set_footer(text = 'Aki © 2022 Все права защищены', icon_url = avatar(self.bot.user))
-            emb.set_author(name = 'Ошибка', icon_url = avatar(ctx.author))
-            if imgs:
-                emb.set_thumbnail(url = imgs['no_permissions'])
-            await ctx.send(embed = emb)
-            logger.error(f'Не удалось изменить статус - Причина: Недостаточно прав - Пользователь: {ctx.author} ({ctx.author.id}).')
+            await Errors.no_permissions_embed(self, ctx)
+            logger.error(f'Не удалось изменить статус - Причина: Недостаточно прав — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
 
+
+    @botstatus_command.error
+    async def botstatus_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await Errors.custom_msg_embed(self, ctx, error)
+            logger.error(f'Не удалось изменить статус - Причина: {error} — Запросил пользователь: {ctx.author} ({ctx.author.id}).')
+            logger.error(error)
+        else:
+            await Errors.custom_msg_embed(self, ctx, error)
+            logger.error(error)
 def setup(bot):
     bot.add_cog(BotStatus(bot))
